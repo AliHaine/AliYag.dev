29f2be611a38098fd0db89673c936ba4d2c344dc
AliHaine <alitr57120@gmail.com>
2025-04-05 01:54:42 +0200

    Start to setup principal div, colors and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-              <router-outlet></router-outlet>
+                <router-outlet></router-outlet>
             `,
 })
 export class AppComponent {
