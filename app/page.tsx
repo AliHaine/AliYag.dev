f613b363363bc7a584471a1851f81b7bb32de35a
AliHaine <alitr57120@gmail.com>
2025-04-07 00:01:28 +0200

    Update top area

--- app/page.tsx
+++ app/page.tsx
@@ -3,93 +3,53 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className={styles.main}>
+      <div id={styles.top}>
+        <div id={styles.topContent}>
+          <div id={styles.topLeftContent}>
+            <h1 id={styles.mainTitle}>Ali YAGMUR</h1>
+            <h3 id={styles.subTitle}>Developer and AI engineer</h3>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <div id={styles.stats}>
+              <div className={styles.stat}>
+                <div className={styles.statTitle}>Github contributions</div>
+                <div>547</div>
+              </div>
+              <div className={styles.stat}>
+                <div className={styles.statTitle}>Github stars</div>
+                <div>17</div>
+              </div>
+              <div className={styles.stat}>
+                <div className={styles.statTitle}>Servers running</div>
+                <div>67</div>
+              </div>
+              <div className={styles.stat}>
+                <div className={styles.statTitle}>Blog posts</div>
+                <div>7</div>
+              </div>
+            </div>
+
+          </div>
+
+          <div id={styles.topRightContent}>
+            <p id={styles.topDescription}>Lorem ipsum dolor sit amet. Eos dicta numquam non fugiat voluptatibus et
+              quibusdam minus est explicabo assumenda. In
+              animi quod ab mollitia corporis est officia quaerat ut pariatur
+              <br/>
+              <br/>
+              explicabo vel itaque itaque aut sunt voluptatem non quae officia.
+              Et provident pariatur At veniam nihil sed ullam quia ut galisum aperiam et possimus impedit ut iure
+              inventore ad rerum
+              <br/>
+              <br/>
+              laudantium. Est omnis quisquam sed accusamus galisum ex sapiente repellendus ut sint dolorum id similique
+              dolores est impedit harum et tempora fuga.</p>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div id={styles.bot}>
+
+      </div>
+    </main>
   );
 }
